5cc5032605e1e3d171103f7089d627ff2aa612bb
computer17work-beep <computer17work@gmail.com>
2026-08-05 19:12:03 +0600

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,73 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Lord Voice"),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              const Icon(
+                Icons.graphic_eq_rounded,
+                size: 90,
+                color: Color(0xFF3B82F6),
+              ),
+
+              const SizedBox(height: 20),
+
+              const Text(
+                "LORD VOICE",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+              const Text(
+                "Premium Voice Chat",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+
+              const SizedBox(height: 60),
+
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("CREATE ROOM"),
+              ),
+
+              const SizedBox(height: 20),
+
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 60),
+                  side: const BorderSide(color: Color(0xFF3B82F6)),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                ),
+                child: const Text(
+                  "JOIN ROOM",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+
+              const Spacer(),
+
+              const Text(
+                "Version 0.1",
+                style: TextStyle(color: Colors.white54),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
