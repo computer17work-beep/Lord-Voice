53429a10f8faed27c2f00b3ebf750f5633bdd30f
computer17work-beep <computer17work@gmail.com>
2026-08-05 19:04:28 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const LordVoiceApp());
+}
+
+class LordVoiceApp extends StatelessWidget {
+  const LordVoiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
